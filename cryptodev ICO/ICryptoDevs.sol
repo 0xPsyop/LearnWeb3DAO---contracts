780053c5d391a5502f38f3a0a45186f6894b5794
Manujaya <73481420+Iam-SilentEyes@users.noreply.github.com>
2022-08-14 00:10:28 +0530

    Delete cryptodev ICO directory

--- cryptodev ICO/ICryptoDevs.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.4;
-
-interface ICryptoDevs {
-  
-  function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
-  function balanceOf(address owner) external view returns (uint256);
-}
